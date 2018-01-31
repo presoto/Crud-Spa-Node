2259f57a9553a84944c3e3edcf56ee46071543ee
gabriel presoto <gabrielpresotocel@gmail.com>
2018-01-31 14:57:44 -0200

    mudando da porta 8080 para 5000 / heroku

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-server.listen(8080, function () {
+server.listen(5000, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
 
